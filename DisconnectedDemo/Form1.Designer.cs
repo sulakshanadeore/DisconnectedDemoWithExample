534199bd707e52bbdc31e39a94dcd7c81c8019eb
deoresulakshana <deoresulakshana@gmail.com>
2023-11-10 11:09:08 +0530

    Insert Record

--- DisconnectedDemo/Form1.Designer.cs
+++ DisconnectedDemo/Form1.Designer.cs
@@ -29,6 +29,14 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtskillid = new System.Windows.Forms.TextBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txttype = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,23 +48,111 @@
             this.dataGridView1.Size = new System.Drawing.Size(404, 179);
             this.dataGridView1.TabIndex = 0;
             // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(65, 276);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(89, 39);
+            this.btnInsert.TabIndex = 1;
+            this.btnInsert.Text = "Insert Record";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(338, 312);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // txtskillid
+            // 
+            this.txtskillid.Location = new System.Drawing.Point(105, 49);
+            this.txtskillid.Name = "txtskillid";
+            this.txtskillid.ReadOnly = true;
+            this.txtskillid.Size = new System.Drawing.Size(100, 20);
+            this.txtskillid.TabIndex = 3;
+            this.txtskillid.MouseHover += new System.EventHandler(this.txtskillid_MouseHover);
+            this.txtskillid.MouseMove += new System.Windows.Forms.MouseEventHandler(this.txtskillid_MouseMove);
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(105, 100);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(100, 20);
+            this.txtname.TabIndex = 4;
+            this.txtname.TextChanged += new System.EventHandler(this.txtname_TextChanged);
+            // 
+            // txttype
+            // 
+            this.txttype.Location = new System.Drawing.Point(105, 142);
+            this.txttype.Name = "txttype";
+            this.txttype.Size = new System.Drawing.Size(100, 20);
+            this.txttype.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Enter SkillID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Enter SkillName";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Enter SkillType";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txttype);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.txtskillid);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnInsert);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtskillid;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txttype;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- DisconnectedDemo/Form1.cs
+++ DisconnectedDemo/Form1.cs
@@ -19,6 +19,7 @@ namespace DisconnectedDemo
 
         private void Form1_Load(object sender, EventArgs e)
         {
+          
             SqlConnection cn = new SqlConnection("server=Sulakshana\\sqlexpress;Integrated Security=true;database=Northwind");
             SqlDataAdapter da = new SqlDataAdapter("select * from TechSkills", cn);
             DataSet ds = new DataSet();
@@ -29,5 +30,58 @@ namespace DisconnectedDemo
             dt=ds.Tables["TechSkills"];
             dataGridView1.DataSource = dt;
             }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            SqlConnection cn = new SqlConnection("server=Sulakshana\\sqlexpress;Integrated Security=true;database=Northwind");
+            SqlDataAdapter da = new SqlDataAdapter("select * from TechSkills", cn);
+            DataSet ds = new DataSet();
+            da.MissingSchemaAction = MissingSchemaAction.AddWithKey;//PrimaryKey/Foreign key
+            da.Fill(ds, "TechSkills");//After execution of this line, database is disconnected
+
+           DataRow myrow= ds.Tables["TechSkills"].NewRow();
+
+            myrow["SkillName"] = txtname.Text.Trim();
+            myrow["SkillType"] = txttype.Text.Trim();
+
+            ds.Tables["TechSkills"].Rows.Add(myrow);
+
+            //Reconnect to db server--submitting the changes
+            SqlCommandBuilder bldr = new SqlCommandBuilder(da);//insert
+            da.Update(ds.Tables["TechSkills"]);
+            MessageBox.Show("Added Successfully...");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private void txtskillid_MouseMove(object sender, MouseEventArgs e)
+        {
+  
+        }
+
+        private void txtskillid_MouseHover(object sender, EventArgs e)
+        {
+            txtskillid.ReadOnly=false;
+        }
+
+        private void txtname_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
     }
 }
