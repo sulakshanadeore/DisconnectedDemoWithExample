292972f7eec964bf5db74ba14f3487d736481608
deoresulakshana <deoresulakshana@gmail.com>
2023-11-10 10:27:19 +0530

    Add project files.

---
+++ DisconnectedDemo/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace DisconnectedDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection cn = new SqlConnection("server=Sulakshana\\sqlexpress;Integrated Security=true;database=Northwind");
+            SqlDataAdapter da = new SqlDataAdapter("select * from TechSkills", cn);
+            DataSet ds = new DataSet();
+            da.MissingSchemaAction = MissingSchemaAction.AddWithKey;//PrimaryKey/Foreign key
+            da.Fill(ds,"TechSkills");//After execution of this line, database is disconnected
+
+            DataTable dt = new DataTable();
+            dt=ds.Tables["TechSkills"];
+            dataGridView1.DataSource = dt;
+            }
+    }
+}
